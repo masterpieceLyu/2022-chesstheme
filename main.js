c53eab9c53bd8b8c26e24eb8b3254da5fb579fe1
masterpieceLyu <elzkdntm@naver.com>
2022-04-12 19:26:55 +0900

    finished main section1

---
+++ main.js
@@ -0,0 +1,25 @@
+const mainText1 = document.querySelector("main-main__text__1");
+const mainText2 = document.querySelector("main-main__text__2");
+const mainText3 = document.querySelectorAll("main-main__text__3");
+
+/*
+window.addEventListener("scroll", function () {
+  let value = this.scrollY;
+
+  if (value > 350) {
+    maintext.style.animation = "slideback 1.3s ease-in-out forwards";
+  } else {
+    maintext.style.animation = "leftslide 1.3s ease-in-out";
+  }
+});
+
+window.addEventListener("scroll", function () {
+  let value = this.scrollY;
+
+  if (value > 350) {
+    maintext2.style.animation = "slideback 1.3s ease-in-out forwards";
+  } else {
+    maintext2.style.animation = "leftslide 1.3s ease-in-out";
+  }
+});
+*/
