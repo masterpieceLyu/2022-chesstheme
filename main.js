bae37e952fc0c6adaf719985f73f7527717d30a5
masterpieceLyu <elzkdntm@naver.com>
2022-04-13 19:07:23 +0900

    add 2nd main and need menu JS animation

--- main.js
+++ main.js
@@ -1,25 +1,59 @@
-const mainText1 = document.querySelector("main-main__text__1");
-const mainText2 = document.querySelector("main-main__text__2");
-const mainText3 = document.querySelectorAll("main-main__text__3");
+const mainText1 = document.querySelector(".main-main__text__1");
+const mainText2 = document.querySelector(".main-main__text__2");
+const mainText3 = document.querySelectorAll(".main-main__text__3");
 
-/*
-window.addEventListener("scroll", function () {
+const mainText4 = document.querySelector(".main-main2__text__1");
+const mainText5 = document.querySelector(".main-main2__text__2");
+const mainText6 = document.querySelector(".main-main2__text__3");
+const mainText7 = document.querySelector(".main-main2__text__4");
+const movingCrown = document.querySelector(".movingCrown");
+const pointOn = document.querySelector(".pointOn");
+
+/* ------ 1vh = 884px ------ */
+
+addEventListener("scroll", function () {
   let value = this.scrollY;
+  console.log(value);
 
-  if (value > 350) {
-    maintext.style.animation = "slideback 1.3s ease-in-out forwards";
+  if (value > 442) {
+    mainText4.style.display = "inline";
+    mainText5.style.display = "inline";
+    mainText6.style.display = "inline";
+    mainText7.style.display = "inline";
+    movingCrown.style.display = "block";
+
+    mainText4.style.animation = "slideMainText 1.5s ease-in-out forwards";
+    mainText5.style.animation = "slideMainText 1.5s 0.2s ease-in-out forwards";
+    mainText6.style.animation = "slideMainText 1.5s 0.4s ease-in-out forwards";
+    mainText7.style.animation = "slideMainText 1.5s 0.4s ease-in-out forwards";
+    movingCrown.style.animation =
+      "apear 0.8s 1.3s ease forwards, movingCrown 7s 2s ease-in-out infinite";
   } else {
-    maintext.style.animation = "leftslide 1.3s ease-in-out";
+    mainText4.style.display = "none";
+    mainText5.style.display = "none";
+    mainText6.style.display = "none";
+    mainText7.style.display = "none";
+    movingCrown.style.display = "none";
+
+    mainText4.style.animation = "slideMainTextDisapear 1.5s ease-in-out";
+    mainText5.style.animation = "slideMainTextDisapear 1.5s ease-in-out";
+    mainText6.style.animation = "slideMainTextDisapear 1.5s ease-in-out";
+    mainText7.style.animation = "slideMainTextDisapear 1.5s ease-in-out";
+    movingCrown.style.animation = "disApear 0.8s ease forwards";
   }
 });
 
-window.addEventListener("scroll", function () {
-  let value = this.scrollY;
+const nav_menu = document.querySelector(".header__nav-menu");
+const nav_menu_hidden = document.querySelector(".header__nav-menu-hidden");
 
-  if (value > 350) {
-    maintext2.style.animation = "slideback 1.3s ease-in-out forwards";
-  } else {
-    maintext2.style.animation = "leftslide 1.3s ease-in-out";
+nav_menu.addEventListener("click", handleClick);
+
+function handleClick() {
+  nav_menu.classList.toggle("active");
+}
+
+function name() {
+  if ((nav_menu_hidden.classList.toggle = "active")) {
+    nav_menu_hidden.style.display("flex");
   }
-});
-*/
+}
